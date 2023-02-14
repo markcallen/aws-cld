66c025e500967cde98e440e92790ce6e269a92f1
Mark C Allen <mark@markcallen.com>
2023-02-13 18:57:47 -0800

    Added role for external-dns service account to modify route53

---
+++ env/modules/eks/externaldns.tf
@@ -0,0 +1,58 @@
+resource "aws_iam_policy" "externaldns" {
+  name        = "externaldns-policy-${local.name}"
+  description = format("Allow externaldns to manage route53 resources")
+  path        = "/"
+  policy      = <<EOT
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Action": [
+        "route53:ChangeResourceRecordSets"
+      ],
+      "Resource": [
+        "arn:aws:route53:::hostedzone/*"
+      ]
+    },
+    {
+      "Effect": "Allow",
+      "Action": [
+        "route53:ListHostedZones",
+        "route53:ListResourceRecordSets"
+      ],
+      "Resource": [
+        "*"
+      ]
+    }
+  ]
+}
+EOT
+}
+
+resource "aws_iam_role" "externaldns_irsa" {
+  name = "externaldns-irsa-${local.name}"
+  assume_role_policy = jsonencode({
+    Statement = [{
+      Action = "sts:AssumeRoleWithWebIdentity"
+      Effect = "Allow"
+      Principal = {
+        Federated = module.eks.oidc_provider_arn
+      }
+      Condition = {
+        StringEquals = {
+          "${module.eks.oidc_provider}:sub" = "system:serviceaccount:${local.namespace}:external-dns"
+        }
+      }
+    }]
+    Version = "2012-10-17"
+  })
+  tags = local.tags
+}
+
+resource "aws_iam_role_policy_attachment" "externaldns_irsa_attachment" {
+  policy_arn = aws_iam_policy.externaldns.arn
+  role       = aws_iam_role.externaldns_irsa.name
+}
+
+
