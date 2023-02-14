1ce1cbaaa259d089f13b16407df1bd24584cb834
Mark C Allen <mark@markcallen.com>
2023-02-13 17:10:27 -0800

    Added version

--- infra/iam/versions.tf
+++ infra/iam/versions.tf
@@ -1,7 +1,11 @@
 terraform {
   required_providers {
     aws = {
-      source = "hashicorp/aws"
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+    local = {
+      source = "hashicorp/local"
     }
   }
   required_version = ">= 0.13"
