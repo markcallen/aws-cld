698fc13a501cde7099a6d13ec2fbb98b88b8c078
Mark C Allen <mark@markcallen.com>
2023-02-16 18:31:16 -0800

    Added us_west output

--- env/ec2/outputs.tf
+++ env/ec2/outputs.tf
@@ -4,3 +4,9 @@ output "inventory_us_east" {
 output "dns_names_us_east" {
   value = module.ec2_us_east.dns_names
 }
+output "inventory_us_west" {
+  value = module.ec2_us_west.inventory
+}
+output "dns_names_us_west" {
+  value = module.ec2_us_west.dns_names
+}
