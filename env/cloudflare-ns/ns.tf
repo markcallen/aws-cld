55b84125ee1624ba151cab7b1f0176e205dac443
Mark C Allen <mark@markcallen.com>
2023-07-02 19:23:33 -0700

    Removed tags

--- env/cloudflare-ns/ns.tf
+++ env/cloudflare-ns/ns.tf
@@ -1,11 +1,3 @@
-locals {
-  common_tags = [
-    "Environment: ${var.environment}",
-    "ManagedBy: aws-cld",
-    "Project: ${var.project}"
-  ]
-}
-
 resource "cloudflare_record" "aws-ns-record" {
   zone_id  = var.zone_id
   count    = length(var.name_servers)
@@ -13,6 +5,4 @@ resource "cloudflare_record" "aws-ns-record" {
   value    = element(var.name_servers, count.index)
   type     = "NS"
   priority = 1
-
-  tags = local.common_tags
 }
--- env/cloudflare-ns/variables.tf
+++ env/cloudflare-ns/variables.tf
@@ -1,6 +1,3 @@
-variable "project" {
-  type = string
-}
 variable "environment" {
   type = string
 }
